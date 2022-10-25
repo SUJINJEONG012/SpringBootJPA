142077292ca072f7403f63746b630a06bf55fe36
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-25 23:44:10 +0900

    유효성검사 컨트롤러

--- src/main/java/com/mysite/project/controller/QuestionController.java
+++ src/main/java/com/mysite/project/controller/QuestionController.java
@@ -2,18 +2,20 @@ package com.mysite.project.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mysite.project.model.Question;
 import com.mysite.project.service.QuestionService;
 
 import lombok.RequiredArgsConstructor;
-
-
 
 @RequiredArgsConstructor
 @Controller
@@ -23,6 +25,29 @@ public class QuestionController {
 	//생성자 주입
 	private final QuestionService questionService;
 	
+	//질문 등록하기
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question/question_form";
+	}
+	
+	//질문등록 POST 요청을 처리할 수 있도록 다음처럼 컨트롤러를 수정
+	
+	@PostMapping("/create")
+	
+	/*
+	 * @Valid 애너테이션을 적용하면 QuestionForm의 @NotEmpty, @Size 등으로 설정한 검증 기능이 동작한다.
+	 * */
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question/question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+	
+	
+	
 	//질문 리스트보기
 	@GetMapping("/list")
 	public String list(Model model) {
@@ -30,7 +55,6 @@ public class QuestionController {
 		model.addAttribute("questionList", questionList);
 		return "question/question_list";
 	}
-	
 	
 	/* 질문 상세보기
 	 * 
