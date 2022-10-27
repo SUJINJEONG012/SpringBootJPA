3f031ddf392b130d2643d5ba0962981a39592226
SUJINJEONG012 <peekaboo32@naver.com>
2022-10-27 21:18:47 +0900

    서비스단에 객체생성대신  빈으로 등록한 PasswordEncoder 객체를 주입받아 사용하도록 수정

---
+++ src/main/java/com/mysite/project/service/UserService.java
@@ -0,0 +1,35 @@
+package com.mysite.project.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.mysite.project.model.User;
+import com.mysite.project.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+	
+	//User 서비스에는 User 리포지터리를 사용하여 User 데이터를 생성하는 create 메서드를
+	
+	public User create(String username, String email, String password) {
+	
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(email);
+		/*
+		 * BCryptPasswordEncoder 객체를 직접 생성하여 사용하지 않고 
+		 * 빈으로 등록한 PasswordEncoder 객체를 주입받아 사용하도록 수정했다. => 시큐리티 부분에 생성
+		 * */
+		user.setPassword(passwordEncoder.encode(password));
+		this.userRepository.save(user);
+		return user;
+		
+	}
+}
